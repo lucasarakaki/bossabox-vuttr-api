24f7b717ed9ebfeb77f4ec5d7f6a643f830fc837
lucasarakaki <takeshioficial01@gmail.com>
2025-09-03 20:39:09 -0300

    feat: Update DatabaseSeeder to include Tool model seeding and restore user creation logic

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,11 @@ declare(strict_types = 1);
 
 namespace Database\Seeders;
 
+use App\Models\Tool;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +17,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Tool::factory(10)->create();
         User::factory(10)->create();
 
-        /* User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]); */
+        User::factory()->create([
+            'name'       => 'Test User',
+            'email'      => 'test@example.com',
+            'password'   => Hash::make('password'),
+            'created_at' => now(),
+        ]);
     }
 }
