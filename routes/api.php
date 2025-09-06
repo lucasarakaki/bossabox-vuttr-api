6619aeda9da4e409576f39aaf0a025647e265dd2
lucasarakaki <takeshioficial01@gmail.com>
2025-09-05 22:52:21 -0300

    feat: Refactor API routes to group versioning and enhance middleware structure

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
-use Illuminate\Http\Request;
+declare(strict_types = 1);
+
+use App\Http\Controllers\Api\v1\ToolController;
+use App\Http\Controllers\Api\v1\UserController;
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(fn () => [
+
+    Route::middleware(['api', 'auth:sanctum'])->group(fn () => [
+        Route::apiResource('users', UserController::class),
+        Route::apiResource('tools', ToolController::class),
+        Route::post('logout', [AuthController::class, 'logout'])->name('logout'),
+    ]),
+
+    Route::post('login', [AuthController::class, 'login'])->name('login'),
+]);
